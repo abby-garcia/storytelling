2bbe1662a0da009536911267e30e83ca37cd0acb
abby-garcia <acg361@gmail.com>
2015-12-07 17:03:23 -0500

    js story

---
+++ story.js
@@ -0,0 +1,11 @@
+var noun = prompt("Tell me a noun.");
+var verb = prompt("Name a verb.");
+var adjective = prompt("Give me an adjective!");
+
+var final = "Once upon a time, there was a " + noun + " who loved to " + verb + " cats down the " + adjective + " hill.";
+console.log(final);
+
+
+// ask for a noun, verb, and adjective 
+
+// then make it fill in a story
